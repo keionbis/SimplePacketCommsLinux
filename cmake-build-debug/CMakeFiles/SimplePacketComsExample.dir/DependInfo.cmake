8372843cab7b67f1d4b78d07d1cb539768ffc0c6
keionbis <keion96@gmail.com>
2019-12-01 00:13:18 -0500

    Start to a linux implimentation of simplePacketComms using cmake

---
+++ cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kbisland/SimplePacketComsExample/libs/SimplePacketComs/src/PacketEvent.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/PacketEvent.cpp.o"
+  "/home/kbisland/SimplePacketComsExample/libs/SimplePacketComs/src/SimplePacketComs.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/SimplePacketComs.cpp.o"
+  "/home/kbisland/SimplePacketComsExample/libs/SimplePacketComs/src/client/AbstractSimpleComsDevice.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/client/AbstractSimpleComsDevice.cpp.o"
+  "/home/kbisland/SimplePacketComsExample/libs/SimplePacketComs/src/client/PacketType.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/client/PacketType.cpp.o"
+  "/home/kbisland/SimplePacketComsExample/libs/UDPSimplePacket/UDPSimplePacket.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/libs/UDPSimplePacket/UDPSimplePacket.cpp.o"
+  "/home/kbisland/SimplePacketComsExample/main.cpp" "/home/kbisland/SimplePacketComsExample/cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SimplePacketComsExample.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimplePacketComsExample.dir/main.cpp.o"
+  "CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/PacketEvent.cpp.o"
+  "CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/SimplePacketComs.cpp.o"
+  "CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/client/AbstractSimpleComsDevice.cpp.o"
+  "CMakeFiles/SimplePacketComsExample.dir/libs/SimplePacketComs/src/client/PacketType.cpp.o"
+  "CMakeFiles/SimplePacketComsExample.dir/libs/UDPSimplePacket/UDPSimplePacket.cpp.o"
+  "SimplePacketComsExample.pdb"
+  "SimplePacketComsExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimplePacketComsExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
